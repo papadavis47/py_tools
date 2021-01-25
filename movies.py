729710766f59295e1d3cf1aaf26f7f60b763cef4
John William Davis <jwdavisdeveloper@gmail.com>
2021-01-25 13:53:06 -0800

    Initial commit, set psuedo code on first tool

---
+++ movies.py
@@ -0,0 +1,26 @@
+# A python script for making inventory of the films that I own.
+
+import os
+import sys
+
+# Setup three dicts to append to - based on type of copy
+
+dvds = {}
+
+blu_rays = {}
+
+digital = {}
+
+# Welcome user and show what they already have in their list of movies
+
+# Ask user whether they want to enter a DVD, Blu-Ray or a digital version
+
+# Prompt user to input a title and save in a variable
+
+# Prompt user to input the director of that film
+
+# Append the film title and director to the appropriate dict
+
+# Append to a file - wwith fstrings - the information collected in the dicts
+
+# Format the output and save it to the file.
